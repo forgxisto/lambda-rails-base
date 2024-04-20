6f428b401e2bfb6f91ee5e74b1ed0e9d5172d457
ikad <tokunaga.c.daigo@forgxisto.com>
2024-04-20 04:05:09 +0000

    update lambdakiq development log to stdout

--- config/initializers/lambdakiq.rb
+++
@@ -1,9 +0,0 @@
-if Rails.env.development?
-  Rails.application.configure do
-    config.after_initialize do
-      config.active_job.verbose_enqueue_logs = true
-      config.active_job.logger = config.logger
-      config.lambdakiq.metrics_logger = config.logger
-    end
-  end
-end
