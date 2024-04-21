5f72f55590e83fcd23db4ac65753ccab4af2a22e
ikad <tokunaga.c.daigo@forgxisto.com>
2024-04-21 08:55:14 +0000

    add vite_rails

---
+++ app/assets/entrypoints/application.js
@@ -0,0 +1,29 @@
+// To see this message, add the following to the `<head>` section in your
+// views/layouts/application.html.erb
+//
+//    <%= vite_client_tag %>
+//    <%= vite_javascript_tag 'application' %>
+console.log('Vite ⚡️ Rails')
+
+// If using a TypeScript entrypoint file:
+//     <%= vite_typescript_tag 'application' %>
+//
+// If you want to use .jsx or .tsx, add the extension:
+//     <%= vite_javascript_tag 'application.jsx' %>
+
+console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify.app/guide/rails')
+
+// Example: Load Rails libraries in Vite.
+//
+// import * as Turbo from '@hotwired/turbo'
+// Turbo.start()
+//
+// import ActiveStorage from '@rails/activestorage'
+// ActiveStorage.start()
+//
+// // Import all channels.
+// const channels = import.meta.globEager('./**/*_channel.js')
+
+// application.css ファイルを最初に読み込む
+import.meta.glob('../stylesheets/application.css', { eager: true });
+// その他必要な css や js を読み込む
